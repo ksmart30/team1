c99ad0ecd9612f5808eb9c4ee48d88086f889fbe
Agenere <kiiany@naver.com>
2019-03-29 11:26:20 +0900

    [team1-김동열] 외주비관리 detail: <-Controller-> DB 접속 및 결과값 TEST위해 com.cafe24.ksmart30.test.controller 패키지내 TestRestController.java TestRestMapper.java TestRestMapper.xml 추가 - 테스트 완료후 삭제 예정
    <-View->
    /project/last/search.html
    ajax 통해 결과값 출력 확인 TEST

---
+++ ksmart30/src/main/java/com/cafe24/ksmart30/test/controller/TestRestController.java
@@ -0,0 +1,22 @@
+package com.cafe24.ksmart30.test.controller;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestRestController {
+	@Autowired TestRestMapper testRestMapper;
+
+	@GetMapping("/test/search")
+	public List<Map> search(String pjtCd) {
+		System.out.println("테스트실행");
+		List<Map> test = testRestMapper.testSelect(pjtCd);
+		System.out.println("test"+test.toString());
+		return test;		
+	}
+	
+}
