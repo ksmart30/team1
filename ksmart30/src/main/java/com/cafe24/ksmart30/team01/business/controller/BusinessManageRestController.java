bc24b0419a1020c899c5d131d78110d123b10985
MoonHyogyun <gyrbs15@gmail.com>
2019-04-02 16:20:52 +0900

    [team01,문효균] 사업수행결정 승인 현황 - 전체조회구현
    detail:
    <-Controller->
    BusinessManageRestController.businessManageList()메서드 생성
    ajax구현하기위해 RestController 사용
    
    <-Mapper->
    생성
    BusinessManageMapper.java
    BusinessManageMapper.xml
    사업수행결정 승인 현황 전체조회 쿼리 생성
    
    <--vo-->
    BusinessMarketVO.java
    전체조회후 데이터가 잘들어오는지 확인되어 다음작업에는 조건
    을 추가하여 조회하기위해 Vo생성
    
    <-Model->
    BusinessManageService.java
    매퍼에 접근 및 조건검색에 필요한 구체적기능을 실행하기위해 businessManageSelect()메서드 생성
    
    <-View->
    /business/manage/list
    ajax방식으로 리스트를 보여주기위해 기존코드에서 ajax코드 추가

--- ksmart30/src/main/java/com/cafe24/ksmart30/team01/business/controller/BusinessManageRestController.java
+++ ksmart30/src/main/java/com/cafe24/ksmart30/team01/business/controller/BusinessManageRestController.java
@@ -1,55 +1,31 @@
 package com.cafe24.ksmart30.team01.business.controller;
 
-import org.springframework.ui.Model;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cafe24.ksmart30.team01.business.service.BusinessManageService;
+import com.cafe24.ksmart30.team01.business.vo.BusinessMarketVO;
+
 @RestController
 public class BusinessManageRestController {
+	@Autowired
+	BusinessManageService businessManageService;
 	
-
-	//2.1.1 사업수행 결정서 검색 List
-	@PostMapping("/business/manage/search")
-	public String businessManageSearch(Model model) {
-		return "business/manage/search";
-	}
-
-
-	//2.1.1 사업수행 결정서 입력 처리
-	@PostMapping("/business/manage/in")
-	public String businessManageIn(Model model) {
-		return "business/manage/in";
-	}
-
-
-
-	//2.1.1 사업수행 결정서 수정 처리
-	@PostMapping("/business/manage/up")
-	public String businessManageUp(Model model) {
-		return "business/manage/up";
-	}
-
-
-	//2.1.2 사업수행 결정서 승인 검색 List
-	@PostMapping("/business/manage/seungin_search")
-	public String businessManageSeunginSearch(Model model) {
-		return "business/manage/seungin_search";
-	}
-
-
-	//사업수행 결정서 승인 처리
-	@PostMapping("/business/manage/seungin")
-	public String businessManageSeungin(Model model) {
-		return "business/manage/seungin";
-	}
-
 	//사업성검토 List
 	@PostMapping("/business/manage/list")
-	public String businessManageList(Model model) {
-		return "business/manage/list";
+	public List<Map<String,Object>> businessManageList(BusinessMarketVO vo) {
+		System.out.print("사업수행결정서list 검색 BusinessManageRestController 실행");
+		System.out.print("BusinessMarketVO 담긴값 : "+vo);
+		List<Map<String,Object>> re = businessManageService.businessManageSelect();
+		
+		return re;
 	}
-
+	
 	
 }
 
