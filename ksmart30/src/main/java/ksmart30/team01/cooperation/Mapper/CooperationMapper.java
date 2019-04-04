3d27883e6daeaedcf3eb24059e6240a25767ff24
Agenere <kiiany@naver.com>
2019-04-04 17:21:53 +0900

    [team01 김동열] 외주관리 - 외주계약서 입력
    datail :
    
    <-Service->
    cooperationPJTYesanSelect
    -
    PJT 에 따른 외주계약서 리스트 요청 위해 mapper에서
    cooperationYesanSelect 메서드 호출 추가
    
    <-Mapper->
    3.1.1 외주계약서 입력 - 조회
    외주관리 - 외주계약서관리 - 외주계약서 입력 - PJT 에 따른 예산현황 리스트 요청
    추상 메서드및 SELECT 쿼리 문 추가
    List<Map<String,Object>> cooperationYesanSelect(String PJT_CD);
    <select id="cooperationPJTPM"
    		parameterType="String"
    		resultType="java.util.Map">
    
    <-view->
    ajax 로 예산현황에 출력 해줌
    <tbody id="yshh">

--- ksmart30/src/main/java/ksmart30/team01/cooperation/Mapper/CooperationMapper.java
+++ ksmart30/src/main/java/ksmart30/team01/cooperation/Mapper/CooperationMapper.java
@@ -1,5 +1,5 @@
 /*
- * @file     ksmart30.team01.cooperation.Mapper.CooperationMapper.java
+ * @file     com.cafe24.ksmart30.team01.cooperation.Mapper.CooperationMapper.java
  * @brief    cooperation Mapper
  * @author   ksmart30 김동열
  */
@@ -14,10 +14,13 @@ import ksmart30.team01.cooperation.domain.CooperationSearchRequest;
 
 @Mapper
 public interface CooperationMapper {
-	
+	//3.1.2 외주계약서 검색
 	// 외주관리 - 외주계약서관리 - 외주계약서 검색 - 리스트 요청
 	List<Map<String,Object>> cooperationList(CooperationSearchRequest vo);
-	
+	// 3.1.1 외주계약서 입력 - 조회
 	// 외주관리 - 외주계약서관리 - 외주계약서 입력 - 1. PJT 에 따른 외주계약서 리스트 요청
-	List<Map<String,Object>> cooperationInPJTSelect(String PJT_CD);
+	List<Map<String,Object>> cooperationPJTSelect(String PJT_CD);
+	// 3.1.1 외주계약서 입력 - 조회
+	// 외주관리 - 외주계약서관리 - 외주계약서 입력 - 2. PJT 에 따른 예산현황 리스트 요청
+	List<Map<String,Object>> cooperationYesanSelect(String PJT_CD);
 }
--- ksmart30/src/main/java/ksmart30/team01/cooperation/service/CooperationService.java
+++ ksmart30/src/main/java/ksmart30/team01/cooperation/service/CooperationService.java
@@ -1,5 +1,5 @@
 /*
- * @file     ksmart30.team01.cooperation.service.CooperationService.java
+ * @file     com.cafe24.ksmart30.team01.cooperation.service.CooperationService.java
  * @brief    cooperation service
  * @author   ksmart30 김동열
  */
@@ -15,26 +15,30 @@ import org.springframework.stereotype.Service;
 import ksmart30.team01.cooperation.Mapper.CooperationMapper;
 import ksmart30.team01.cooperation.domain.CooperationSearchRequest;
 
+
 @Service
 public class CooperationService {
 	@Autowired CooperationMapper cooperationMapper;
 	
+	//3.1.2 외주계약서 검색
 	// 외주관리 - 외주계약서관리 - 외주계약서 검색 - 리스트 요청
 	public List<Map<String,Object>> cooperationSelect(CooperationSearchRequest vo){
 		return cooperationMapper.cooperationList(vo);
 		
 	}
 	
+	// 3.1.1 외주계약서 입력 - 조회
 	// 외주관리 - 외주계약서관리 - 외주계약서 입력 - PJT 에 따른 외주계약서 리스트  및 예산현황 요청 
-	public List<Map<String,Object>> cooperationPJTYesanSelect(String PJT_CD){
+	public Map<String,Object> cooperationPJTYesanSelect(String PJT_CD){
+		System.out.println("cooperationPJTYesanSelect 실행 시작");
 		Map<String,Object> PJTYesanList = new HashMap<String, Object>();
 		//1. PJT 에 따른 외주계약서 리스트 요청
-		List<Map<String,Object>> cooperationSmallList = cooperationMapper.cooperationInPJTSelect(PJT_CD);
+		List<Map<String,Object>> cooperationSmallList = cooperationMapper.cooperationPJTSelect(PJT_CD);
 		PJTYesanList.put("cooperationSmallList", cooperationSmallList);
 		//2. PJT 에 따른 예산현황 리스트 요청
-		List<Map<String,Object>> cooperationYesanList = null;
+		List<Map<String,Object>> cooperationYesanList = cooperationMapper.cooperationYesanSelect(PJT_CD);
 		PJTYesanList.put("cooperationYesanList", cooperationYesanList);
 		//추후 합한 값 리턴값(PJTYesanList) 넘기기
-		return cooperationSmallList;
+		return PJTYesanList;
 	}
 }
