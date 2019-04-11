c843f2075b1b33ef44ac26ccebe5bbcfade01a92
msi753 <msi753@naver.com>
2019-04-11 15:41:55 +0900

    [team1-임명심] 예산서 인건비 및 배부 단가표 조회
    detail:
    제조원가1을 화면에 뿌리기 구현 중
    
    <-Controller->
    baseinfo/controller/DivisionListRestController.java
    예산서 인건비 및 배부 단가표를 조회하기 위한 컨트롤러
    baseinfo/service/DivisionListService.java
    예산서 인건비 및 배부 단가표를 조회하기 위한 서비스
    
    <-Model->
    baseinfo/mapper/DivisionListMapper.xml
    baseinfo/mapper/DivisionListMapper.java
    예산서 인건비 및 배부 단가표를 조회하기 위한 쿼리문
    
    <-View->
    project/projectYesanView.html
    프로젝트 개요가 누적되던 문제 해결
    
    baseInfo/divisionListView.html
    예산서 인건비 및 배부 단가표를 조회함

--- ksmart30/src/main/java/ksmart30/team00/baseinfo/mapper/DivisionMapper.java
+++
@@ -1,8 +0,0 @@
-package ksmart30.team00.baseinfo.mapper;
-
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface DivisionMapper {
-
-}
--- ksmart30/src/main/java/ksmart30/team00/baseinfo/service/DivisionService.java
+++
@@ -1,8 +0,0 @@
-package ksmart30.team00.baseinfo.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class DivisionService {
-
-}
